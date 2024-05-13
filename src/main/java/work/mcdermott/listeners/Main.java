8cf8eeef9fdc3f3bfa38001377961d294d97ac22
Dáire McDermott <dairemcdermott@outlook.com>
2024-05-13 23:57:49 +0100

    initial commit

---
+++ src/main/java/work/mcdermott/listeners/Main.java
@@ -0,0 +1,12 @@
+package work.mcdermott.listeners;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("To be implemented...");
+    }
+
+}
